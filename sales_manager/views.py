01bd9d4798cfdb2482739d443d3f46ed4b7e638a
mkl-by <mkl-by@yandex.ru>
2021-05-19 19:48:40 +0300

    first create db

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def main_page(request):
-    return HttpResponse('Hello, на мою страничку')
+    return render(request, 'sales_manager/index.html')
