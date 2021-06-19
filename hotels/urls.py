a649963e8aaba813baf0777aa1a0a46142f44296
mkl-by <mkl-by@yandex.ru>
2021-06-19 12:06:49 +0300

    room datastart dataend

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 
 from hotels import views
-from hotels.views import HotelsListApiView, RoomsListApiView
+from hotels.views import HotelsListApiView, RoomsListApiView, BookingListApiView
 
 urlpatterns = [
     # path('', views.room_show),
@@ -12,8 +12,8 @@ urlpatterns = [
     path('rating/<int:hotel_id>/<str:name_hotel>/', views.Raiting.as_view(), name='rating'),
     path('rating-ajax/', views.Raiting.as_view()),
     path('api_hotels/', HotelsListApiView.as_view()),
-    path('api/<int:hotel_id>/', RoomsListApiView.as_view()),
-
+    path('api/room/<int:hotel_id>/', RoomsListApiView.as_view()),
+    path('api/<int:hotel_id>/', BookingListApiView.as_view()),
     # path('rating/<int:hotel_id>/<str:name_hotel>/<int:rate>/', views.Raiting1.as_view(), name='ratingone'),
 
     # path('rating/<int:hotel_id>/<str:name_hotel>/<int:type_id>/<int:rat>/', views.Raiting1.as_view(), name='rating_one'),
